03545501b04b5e42455ebdb87ddecf2d856d9c02
cass1618 <cass16180@gmail.com>
2021-03-22 13:10:43 -0700

    Add inital html, js, readme file
    co-authored-by: Min Chang <minchangmhc@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const varName1 = parseInt(prompt("Enter a number:"));
+const varName2 = parseInt(prompt("Enter another number:"));
+
+alert(add(2,1));
